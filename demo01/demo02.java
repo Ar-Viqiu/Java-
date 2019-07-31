62de19d784f03f32ddcfcc7d19f08165fcc7696b
William-Q-Kin <924557098@qq.com>
2019-07-31 13:40:39 +0800

    两数组元素数相乘。

---
+++ demo01/demo02.java
@@ -0,0 +1,9 @@
+package demo1;
+
+public class demo01 {
+	public static void main(String args[]) {
+		int [] [] agas = new int [4] [7];//声明并创建数组agas。
+		int [] firstArray = agas [0];//声明并创建数组firstArray，并且赋值为agas的第一维元素数。
+		System.out.println(firstArray.length * agas.length);//这样两个数组相乘，就是第一维的元素数乘第二维的元素数。
+	}
+}
