910cd01146c2fe7680bd783c4946e89beb71ba35
Ar-Viqiu <924557098@qq.com>
2019-08-23 23:45:28 +0800

    Create demo.java

---
+++ demo01/终极引用的使用示例/demo.java
@@ -0,0 +1,14 @@
+//终极引用的使用示例。
+
+package inheritanceAndPolymorphism;
+
+class Car{
+	int number = 1234;
+}
+public class demo11{
+	public static void main(String args[]) {
+		final Car mycar= new Car();		//终极变量，引用类型。
+		mycar.number = 8888;		//可以，修改的是number的值，而不是mycar的值。
+		mycar = new Car();		//错误，不能修改mycar本身的值。
+	}
+}
