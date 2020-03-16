0337a8fc550d22e28f8e8bc372a47e582bc8fd7b
Ar-Viqiu <924557098@qq.com>
2020-03-16 19:39:50 +0800

    Create dome.java

---
+++ demo01/创建窗口程序/dome.java
@@ -0,0 +1,20 @@
+package PG.zjq.game;
+
+import javax.swing.JFrame;
+
+/** 
+* @author 作者:Vinsky 
+* @version 创建时间：2020年2月28日 下午9:58:24 
+* 类说明 ：游戏主窗口类
+*/
+public class MyGameFrame extends JFrame{
+	public void frame(){
+		
+	}
+	public static void main(String agrs[]) {
+		JFrame f=new JFrame("窗口");
+		f.setVisible(true);
+		f.setSize(300,300);
+		f.setLocation(500,300);
+	}
+}
