ff73d7fd915e6ef72fa6c7826371bcaad827ef90
Ar-Viqiu <924557098@qq.com>
2020-03-16 19:44:18 +0800

    Update dome.java

--- demo01/创建窗口程序/dome.java
+++ demo01/创建窗口程序/dome.java
@@ -7,14 +7,14 @@ import javax.swing.JFrame;
 * @version 创建时间：2020年2月28日 下午9:58:24 
 * 类说明 ：游戏主窗口类
 */
-public class MyGameFrame extends JFrame{
-	public void frame(){
-		
-	}
-	public static void main(String agrs[]) {
-		JFrame f=new JFrame("窗口");
-		f.setVisible(true);
-		f.setSize(300,300);
-		f.setLocation(500,300);
-	}
+
+
+
+//这是一个最最基础的窗口程序了，定义了主窗口，大小和窗口初始位置。
+
+public static void main(String agrs[]) {
+	JFrame f=new JFrame("窗口");//定义窗口
+	f.setVisible(true);//窗口显示
+	f.setSize(300,300);//窗口大小
+	f.setLocation(500,300);//窗口初始位置
 }
